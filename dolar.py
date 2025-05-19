51e0dc6b6d9709dc5b298009bae9e94faadee425
lusca <costaalveslucas022@gmail.com>
2025-05-19 15:51:24 -0300

    Add files via upload

---
+++ dolar.py
@@ -0,0 +1,4 @@
+dolar = float(input('digite a cotação do dólar: '))
+valor_em_dolar = float(input('digite o valor em reais a ser convertido em dólar: '))
+valor_convertido = valor_em_dolar * dolar
+print(f'O valor convertido em reais é: R$ {valor_convertido: }')
