4b86ae91d3da78976e3ecd4ea3fa12dcd7252a82
lusca <costaalveslucas022@gmail.com>
2025-05-19 22:53:09 -0300

    calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,10 +1,31 @@
-print (f'|{28*'-'}|')
-print ('| Calculadora')
-print (f'|{28*'-'}|')
-print ('| 1 - soma')
-print ('| 2 - subtração')
-print ('| 3 - multiplicação')
-print ('| 4 - divisão')
-print (f'|{28*'-'}|')
-escolha = input ('escolha uma das opções: ')
-n1 = input ('escolha')
+continuar = 's'
+while continuar == 's':
+    print(f'{"-"*30}')
+    print(f'{" Calculadora ":^30}')
+    print(f'{"-"*30}')
+
+    print('| 1 - Soma        |')
+    print('| 2 - Subtração   |')
+    print('| 3 - Multiplicação|')
+    print('| 4 - Divisão     |')
+    print(f'{"-"*30}')
+
+    escolha = int(input('| Escolha uma das opções: '))
+    n1 = float(input('| Digite o primeiro número: '))
+    n2 = float(input('| Digite o segundo número: '))
+
+    if escolha == 1:
+        print(f'| O resultado é: {n1+n2}')
+    elif escolha == 2:
+        print(f'| O resultado é: {n1-n2}')
+    elif escolha == 3:
+        print(f'| O resultado é: {n1*n2}')
+    elif escolha == 4:
+        print(f'| O resultado é: {n1/n2}')
+    else:
+        print('| ERRO. Escolha uma opção válida.')
+
+    continuar = input('| Deseja continuar? (s/n) ')
+    print()
+
+print('--FIM DO PROGRAMA--')
