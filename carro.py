e567ff02c87063a74ade3928bd263424f60a0879
lusca <costaalveslucas022@gmail.com>
2025-05-22 14:06:45 -0300

    Add files via upload

---
+++ carro.py
@@ -0,0 +1,7 @@
+carro = input ('digite o nome do carro: ')
+valor = input ('digite o valor do carro: ')
+consumo = input ('digite o consumo KM/L: ')
+print(f'{"-"*33}')
+print('| carro: ', carro)
+print('| valor:', valor)
+print('| consumo:', consumo)
